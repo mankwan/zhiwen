5a6d55642794b2dc51da3ccbcd7cb0482e53981c
mankwan <582604267@qq.com>
2015-03-26 23:39:16 +0800

    images can be uploaded

--- Public/Js/index.js
+++ Public/Js/index.js
@@ -33,7 +33,60 @@ $(function(){
 			autoOpen:'false',
 		});
 	});
-	$('#birth').datepicker();
+	
 	$('#log input[type=radio]').button();
-	$('#log input[type=title]').tooltip();
+	$(document).tooltip({
+		position:{
+			my:'left center',
+			at:'right+5 center'
+		}
+	});
+	$('#email').autocomplete({
+		source: function(request,response){
+			var hosts=['qq.com','163.com','gmail.com'],
+				term=request.term,
+				name=term,
+				host='',
+				ix=term.indexOf('@'),
+				result=[];
+			if(ix>-1){//当有@的时候，重新分配用户名和域名
+				name=term.slice(0,ix);
+				host=term.slice(ix+1);
+			}
+			if(name){
+				var findedHosts=[];//如果没有输入@和后面的域名，全部提示
+				if(host){
+					findedHosts=$.grep(hosts,function(value,index){
+						return value.indexOf(host)>-1
+					});
+				}else{
+					findedHosts=hosts;
+				}
+				var findedResult=$.map(findedHosts,function(value,index){
+					return name + '@' +value;
+				});
+				result= findedResult;
+			}
+			response(result);
+		},
+		position:{
+			my:'left center',
+			at:'right+100 center'
+		},
+	});
+	$('#birth').datepicker({
+		dateFormat:'yy-mm-dd',
+		//dayNames:['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],
+		//dayNamesShort:['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],
+		dayNamesMin:['日','一','二','三','四','五','六'],
+		showWeek:true,
+		changeMonth:true,
+		changeYear:true,
+		showButtonPanel:'true',
+		currentText:'Today',
+		maxDate:0,
+		//minDate:0,
+		hideIfNoPrevNext:true,
+		yearRange:'1990:2015',
+	});
 });
